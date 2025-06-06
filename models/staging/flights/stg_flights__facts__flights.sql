d116a76f32e238fbe639e35c762ed47aefbbb080
email <ometoff@gmail.com>
2025-06-06 13:36:50 +0300

    инкрементальное обновление дз

--- models/staging/flights/stg_flights__facts__flights.sql
+++ models/staging/flights/stg_flights__facts__flights.sql
@@ -1,7 +1,13 @@
 {{
-  config(
-    materialized = 'table',
+
+config(
+    materialized='incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['flight_id'],
+    merge_update_columns = ['*'],
+    on_schema_change = 'sync_all_columns'
     )
+
 }}
       select
         "flight_id",
@@ -16,5 +22,11 @@
         "actual_arrival"
 
       from {{ source('demo_src', 'flights') }}
+      WHERE
+    -- Инкрементальная фильтрация: берем только последние N дней
+        scheduled_departure >= (select max(scheduled_departure) from {{ source('demo_src', 'flights') }}) - INTERVAL '100 days'
 
+{% if is_incremental() %}
+    AND scheduled_departure > (SELECT MAX(scheduled_departure) FROM {{ this }})
+{% endif %}
     
